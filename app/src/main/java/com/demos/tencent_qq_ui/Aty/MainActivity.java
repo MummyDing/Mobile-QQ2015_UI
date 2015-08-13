522c84d46991318765c193d9a62de9c14132c2fe
MummyDing <mummydingz@gmail.com>
2015-08-13 18:41:57 +0800

    完善菜单布局

--- app/src/main/java/com/demos/tencent_qq_ui/Aty/MainActivity.java
+++ app/src/main/java/com/demos/tencent_qq_ui/Aty/MainActivity.java
@@ -2,6 +2,7 @@ package com.demos.tencent_qq_ui.Aty;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends Activity {
 
@@ -9,6 +10,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //透明状态栏
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        //透明导航栏
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
     }
 
 
